41eee81401563a45e179614b5b0012f6dfc5d180
Hamza Saddouki <hsaddouki@gmail.com>
2017-11-30 15:32:02 +0100

    Add project files.

---
+++ LaCasaRural/Models/Llogater.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LaCasaRural.Models
+{
+    public class Llogater
+    {
+        public int IdLlogater { get; set; }
+        public string NomLlogater { get; set; }
+        public string CognomLlogater { get; set; }
+        public int CodiPostal { get; set; }
+        public string NIF { get; set; }
+    }
+}
