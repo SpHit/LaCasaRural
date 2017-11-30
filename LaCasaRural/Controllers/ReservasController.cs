d7b3da1406ea17e5383e94ba65fed7af53d82a04
Hamza Saddouki <hsaddouki@gmail.com>
2017-11-30 16:36:16 +0100

    Update

---
+++ LaCasaRural/Controllers/ReservasController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using LaCasaRural.Models;
+
+namespace LaCasaRural.Controllers
+{
+    public class ReservasController : Controller
+    {
+        private Context db = new Context();
+
+        // GET: Reservas
+        public ActionResult Index()
+        {
+            var reserves = db.Reserves.Include(r => r.Llogater);
+            return View(reserves.ToList());
+        }
+
+        // GET: Reservas/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Reserva reserva = db.Reserves.Find(id);
+            if (reserva == null)
+            {
+                return HttpNotFound();
+            }
+            return View(reserva);
+        }
+
+        // GET: Reservas/Create
+        public ActionResult Create()
+        {
+            ViewBag.IdLlogater = new SelectList(db.Llogaters, "IdLlogater", "NomLlogater");
+            return View();
+        }
+
+        // POST: Reservas/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "IdReserva,DataEntrada,IdLlogater")] Reserva reserva)
+        {
+            if (ModelState.IsValid)
+            {
+                //if (reserva.DataEntrada < )
+                db.Reserves.Add(reserva);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.IdLlogater = new SelectList(db.Llogaters, "IdLlogater", "NomLlogater", reserva.IdLlogater);
+            return View(reserva);
+        }
+
+        // GET: Reservas/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Reserva reserva = db.Reserves.Find(id);
+            if (reserva == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.IdLlogater = new SelectList(db.Llogaters, "IdLlogater", "NomLlogater", reserva.IdLlogater);
+            return View(reserva);
+        }
+
+        // POST: Reservas/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "IdReserva,DataEntrada,IdLlogater")] Reserva reserva)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(reserva).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.IdLlogater = new SelectList(db.Llogaters, "IdLlogater", "NomLlogater", reserva.IdLlogater);
+            return View(reserva);
+        }
+
+        // GET: Reservas/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Reserva reserva = db.Reserves.Find(id);
+            if (reserva == null)
+            {
+                return HttpNotFound();
+            }
+            return View(reserva);
+        }
+
+        // POST: Reservas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Reserva reserva = db.Reserves.Find(id);
+            db.Reserves.Remove(reserva);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
--- LaCasaRural/Models/Reserva.cs
+++ LaCasaRural/Models/Reserva.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,9 +8,11 @@ namespace LaCasaRural.Models
 {
     public class Reserva
     {
+        [Key]
         public int IdReserva { get; set; }
         public DateTime DataEntrada { get; set; }
-        
+        public DateTime? DataSortida { get; set; }
+
         public int IdLlogater { get; set; }
         public virtual Llogater Llogater { get; set; }
     }
